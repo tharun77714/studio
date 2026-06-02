8b009572095f02d5df46b1ed503ad6ad138d5dcf
tharun77714 <kothapallitharun777@gmail.com>
2026-06-03 00:17:18 +0530

    fix: add proper android pwa icons to trigger install prompt

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -11,9 +11,14 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: '#0a0700',
     icons: [
       {
-        src: '/favicon.ico',
-        sizes: 'any',
-        type: 'image/x-icon',
+        src: '/icon-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        src: '/icon-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
       },
     ],
   }
