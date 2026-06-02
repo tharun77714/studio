c999b1c89ca7d94b7292e2ffb0458a72f1950720
tharun77714 <kothapallitharun777@gmail.com>
2026-06-03 00:25:17 +0530

    Revert "fix: add proper android pwa icons to trigger install prompt"
    This reverts commit 8b009572095f02d5df46b1ed503ad6ad138d5dcf.

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -11,14 +11,9 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: '#0a0700',
     icons: [
       {
-        src: '/icon-192x192.png',
-        sizes: '192x192',
-        type: 'image/png',
-      },
-      {
-        src: '/icon-512x512.png',
-        sizes: '512x512',
-        type: 'image/png',
+        src: '/favicon.ico',
+        sizes: 'any',
+        type: 'image/x-icon',
       },
     ],
   }
